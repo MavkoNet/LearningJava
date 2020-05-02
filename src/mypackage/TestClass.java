6157ffee34292c295ff5ab09fef65b12d77bbd90
ann <anna.minniakhmetova@piano.io>
2020-05-02 16:44:27 +0400

    Data Types

--- src/mypackage/TestClass.java
+++ src/mypackage/TestClass.java
@@ -9,12 +9,35 @@ public class TestClass {
         int i = 100000;
         long l = -55500000;
         double d = 7.7;
+        float f = 13.9f;
+        boolean boo1 = false, boo2 = true;
 
         System.out.println("byte: " + b);
         System.out.println("short: " + s);
         System.out.println("int: " + i);
         System.out.println("long: " + l);
-        System.out.print("double: " + d);
+        System.out.println("double: " + d);
+        System.out.println("float with f(!): " + f);
+        System.out.println(boo1);
+        System.out.println(boo2);
+
+        boolean boo3 = 2 + 4 > 0;
+        boolean boo4 = 2 + 4 > 7;
+
+        System.out.println("Верно ли выражение: '2 + 4 > 0' ? " + boo3);
+        System.out.println("Верно ли выражение: '2 + 4 > 7' ? " + boo4);
+        System.out.println();
+
+        String str1 = "Hello", str2 = "World!";
+        String str = str1 + " " + str2;
+
+        System.out.println(str);
+
+        char ch = 'q';
+
+        System.out.println(ch);
+        String personName;
+        int age;
 
     }
 
