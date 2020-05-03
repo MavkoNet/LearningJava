cfd7077cc13dfc036fbc5c035a356985361b8044
ann <anna.minniakhmetova@piano.io>
2020-05-03 16:42:24 +0400

    data types

--- src/mypackage/TestClass.java
+++ src/mypackage/TestClass.java
@@ -4,40 +4,80 @@ public class TestClass {
 
     public static void main(String[] args) {
 
-        byte b = 5; // -128 127
-        short s = 100; // -35... 35...
-        int i = 100000;
-        long l = -55500000;
-        double d = 7.7;
-        float f = 13.9f;
-        boolean boo1 = false, boo2 = true;
+        byte by = 127;           // -128 127
+        short s = 100;          // -32 768 до 32 767
+        int i = 100000;         //от -2 147 483 648 до 2 147 483 647
+        long l = -55500000;     //от -9223372036854775808 до 9223372036854775807
 
-        System.out.println("byte: " + b);
+        float f = 13.9f;        //от (2 в степени -149) до ((2-2 в степени -23)*2 в степени 127)
+        double d = 7.7;         //от (-2 в степени 63) до ((2 в степени 63)-1)
+
+        char ch = 'q';
+
+        boolean boo1 = true;
+
+        System.out.println("byte: " + by);
         System.out.println("short: " + s);
         System.out.println("int: " + i);
-        System.out.println("long: " + l);
-        System.out.println("double: " + d);
+        System.out.println("long with L(!): " + l);
+
         System.out.println("float with f(!): " + f);
+        System.out.println("double: " + d);
+
+        System.out.println(ch);
+
         System.out.println(boo1);
-        System.out.println(boo2);
+        System.out.println();
 
         boolean boo3 = 2 + 4 > 0;
-        boolean boo4 = 2 + 4 > 7;
-
         System.out.println("Верно ли выражение: '2 + 4 > 0' ? " + boo3);
+
+        boolean boo4 = 2 + 4 > 7;
         System.out.println("Верно ли выражение: '2 + 4 > 7' ? " + boo4);
         System.out.println();
 
         String str1 = "Hello", str2 = "World!";
         String str = str1 + " " + str2;
-
         System.out.println(str);
+        System.out.println();
 
-        char ch = 'q';
+        //Наименование переменных должно нести смысл, имя начинается с маленькой буквы, следующие слова с большой, н/р:
+        //String personName;
+        //int age;
 
-        System.out.println(ch);
-        String personName;
-        int age;
+        //Приведение типов. Есть автоматическое и явное. При явном возможны искажения значения.
+        //Три примера, где допустимо неявное приведение, но возможна потеря точности.
+
+        // int -> float
+        int a1 = 1000090;
+        System.out.println("int -> float: " + (float) a1);
+
+        //long -> float
+        long l1 = 9223372036854775807L;
+        System.out.println("long -> float: " + (float) l1);
+
+        //long -> double
+        long l2 = 9223372036854775807L;
+        System.out.println("long -> double: " + (double) l2);
+        System.out.println();
+
+        //Еще один пример с long и float
+        long orig = 0x7effffff00000000L;
+        float fval = orig;
+        long lose = (long) fval;
+
+        System.out.println("orig = " + orig);
+        System.out.println("fvаl = " + fval);
+        System.out.println("lose = " + lose);
+        System.out.println();
+
+        //Тип Char содержит код символа в цифровом виде
+        short b2 = 65;
+        System.out.println((char) b2);
+
+        //Для выражений существуют особые правила приведения типов
+        byte b1 = 127;
+        int b3 = b1 + 2;
 
     }
 
