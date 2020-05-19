ea7c424cbe677e9bc55000895d96d1d7149cab76
AnnaM <anna.minniakhmetova@piano.io>
2020-05-19 23:38:04 +0400

    IfSwitch

---
+++ src/mypackage/IfSwitch.java
@@ -0,0 +1,103 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class IfSwitch {
+    public static void main(String[] args) {
+
+        /*
+        * Задание.
+        * Введите значение n. По заданному значению n = 1..12 вывести название соответствующего месяца.
+        * При этом, необходимо учесть ошибочные значения n.
+        * Определить число дней в месяце, если месяц задан значением n = 1..12.
+        */
+
+        //Ввод пользователем месяца
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите номер месяца: ");
+
+        //Проверка на корректность введенного значения
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            sc.close();
+            if (n > 0 && n < 13) {
+                System.out.print("Вы ввели число месяца: " + n);
+                System.out.print(". Это ");
+                month(n);
+                amountDays(n);
+            } else {
+                System.out.println("Введите число от 1 до 12");
+            }
+        } else {
+            System.out.println("Вы должны ввести целое число от 1 до 12");
+        }
+
+    }
+
+    //Определение количества дней в месяце
+    public static void amountDays(int num) {
+        if (num == 2 ) {
+            System.out.println(". В этом месяце 29 дней!");
+            return;
+        }
+
+        if ((((num % 2) == 0) && ((1 <= num) && (num <= 7))) || (((num % 2) == 1) && ((7 < num) && (num <= 12)))) {
+            System.out.println(". В этом месяце 30 дней!");
+        } else
+            System.out.println(". В Этом месяце 31 день!");
+
+    }
+
+    //Определение введенного месяца
+    public static void month(int num) {
+        switch (num) {
+            case 1:
+                System.out.print("Январь");
+                return;
+
+            case 2:
+                System.out.print("Февраль");
+                return;
+
+            case 3:
+                System.out.print("Март");
+                return;
+
+            case 4:
+                System.out.print("Апрель");
+                return;
+
+            case 5:
+                System.out.print("Май");
+                return;
+
+            case 6:
+                System.out.print("Июнь");
+                return;
+
+            case 7:
+                System.out.print("Июль");
+                return;
+
+            case 8:
+                System.out.print("Август");
+                return;
+
+            case 9:
+                System.out.print("Сентябрь");
+                return;
+
+            case 10:
+                System.out.print("Октябрь");
+                return;
+
+            case 11:
+                System.out.print("Ноябрь");
+                return;
+
+            case 12:
+                System.out.print("Декабрь");
+
+        }
+    }
+}
